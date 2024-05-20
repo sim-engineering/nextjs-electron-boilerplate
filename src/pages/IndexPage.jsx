98670f05d65c9dc45171e8712aad76cd00e3ad10
Simeon Arabov <s.arabov@myxrobotics.com>
2024-05-20 18:47:33 +0300

    comment out navigation and change lang button

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -117,14 +117,14 @@ const IndexPage = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Navigation />
+      /** <Navigation />
       <button
         onClick={switchLanguage}
         className="m-4 p-2 text-white border border-white rounded-xl"
       >
         {text.language_button[language]}
       </button>
-
+      **/
       <div className="flex-grow px-4 py-8">
         <Quiz questions={questions} language={language} />
       </div>
